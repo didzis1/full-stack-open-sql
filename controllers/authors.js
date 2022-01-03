8580080b8328040e6e1c4e606932483a20a807fa
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2022-01-03 15:29:37 +0200

    13.16

---
+++ controllers/authors.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { Blog } = require('../models');
+const { sequelize } = require('../util/db');
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll({
+    attributes: [
+      'author',
+      [sequelize.fn('sum', sequelize.col('likes')), 'likes'],
+      [sequelize.fn('count', sequelize.col('id')), 'blogs']
+    ],
+    group: 'author'
+  });
+
+  return res.json(blogs);
+});
+
+module.exports = router;
