775d3bff2499fb2d47088c49e9dcb25b81afda1a
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2022-01-04 12:20:48 +0200

    13.19

---
+++ models/readingList.js
@@ -0,0 +1,36 @@
+const { DataTypes, Model } = require('sequelize');
+const { sequelize } = require('../util/db');
+
+class ReadingList extends Model {}
+
+ReadingList.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'users', key: 'id' }
+    },
+    blogId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'blogs', key: 'id' }
+    },
+    isRead: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
+  },
+  {
+    sequelize,
+    underscored: true,
+    modelName: 'readingList'
+  }
+);
+
+module.exports = ReadingList;
