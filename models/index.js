0da7560b4ccac8ed986ca3a25d06dfd5847d3a54
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2022-01-04 11:18:46 +0200

    13.17 migrations

--- models/index.js
+++ models/index.js
@@ -4,7 +4,8 @@ const User = require('./user');
 User.hasMany(Blog);
 Blog.belongsTo(User);
 
-Blog.sync({ alter: true });
-User.sync({ alter: true });
+// Syncing is done by migrations now
+// Blog.sync({ alter: true });
+// User.sync({ alter: true });
 
 module.exports = { Blog, User };
